7376b84da5d186e927b05fc7da28c89cd563068a
alexsunxl <alexsunxl@163.com>
2024-07-03 17:42:13 +0800

    totle percent check

--- src/app/contracts/index.ts
+++ src/app/contracts/index.ts
@@ -6,6 +6,13 @@ async function createWhitelistInvestment(values: StoreValue) {
   // const title = values.title
   // const content = values.content
   //
+  const totlePercent = values.whitelist.reduce((acc: number, cur: any) => {
+    return acc + cur.percent
+  }, 0)
+  if (totlePercent !== 100) {
+    message.error('error: total percent must be 100')
+    return false
+  }
 
   if (!values.endTime) {
     message.error('error: missing end time')
