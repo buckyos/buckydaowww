1580cb9af8e8e182fabeef2c553002df1121875f
alexsunxl <alexsunxl@163.com>
2024-07-25 15:39:30 +0800

    update change commmittee modal

--- src/app/components/modal/ChangeCommitteeModal.tsx
+++ src/app/components/modal/ChangeCommitteeModal.tsx
@@ -1,20 +1,37 @@
+'use client'
 import { Dispatch, SetStateAction, useState } from 'react'
+import { useAsyncEffect } from 'ahooks'
 import { StoreValue } from 'antd/es/form/interface'
 import { Modal, Form, Input, Button, message, Spin } from 'antd'
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons'
 import useContractStore from '@hooks/useContract'
 import useUserStore from '@hooks/useUserStore'
 import { extractMessage, transactionWait } from '@utils/index'
 import { proposalSetExtraAndParams } from '@services/index'
+import { fetchMembers } from '@services/index'
 
 const ChangeCommitteeModal: React.FC<{
   showModal: boolean
   setShowModal: Dispatch<SetStateAction<boolean>>
 }> = ({ showModal, setShowModal }) => {
+  const [committee, setCommittee] = useState<CommitteeMember[]>([])
+  const [loading, setLoading] = useState(false)
+
   const [isSubmitting, setIsSubmitting] = useState(false)
   const contract = useContractStore()
   const user = useUserStore()
 
-  const onCreateProposal = async (values: StoreValue) => {
+  useAsyncEffect(async () => {
+    if (!showModal) {
+      return
+    }
+    setLoading(true)
+    const data = await fetchMembers()
+    setCommittee(data.data)
+    setLoading(false)
+  }, [showModal])
+
+  const onChnageCommitteeProposal = async (values: StoreValue) => {
     console.log('🍻 values :', values)
     setIsSubmitting(true)
     // const fn = async () => {
@@ -70,13 +87,69 @@ const ChangeCommitteeModal: React.FC<{
     >
       <Spin tip='Waiting for confirmation...' spinning={isSubmitting}>
         <Form
-          onFinish={onCreateProposal}
+          onFinish={onChnageCommitteeProposal}
           className='mt-6'
           name=''
           style={{ width: '100%' }}
           autoComplete='off'
         >
           <div className='flex justify-center'>
+            {loading && (
+              <div className='flex-center py-10'>
+                <Spin />
+              </div>
+            )}
+
+            {committee.length > 0 && (
+              <Form.List name='committee' initialValue={committee}>
+                {(fields, { add, remove }) => (
+                  <>
+                    <div>
+                      {fields.map(({ key, name }, index) => {
+                        return (
+                          <div
+                            key={key}
+                            className='flex items-center justify-between gap-10'
+                          >
+                            <Form.Item
+                              className='w-full'
+                              name={[name, 'address']}
+                              rules={[
+                                {
+                                  required: true,
+                                  message: 'committee address is required',
+                                },
+                              ]}
+                            >
+                              <Input placeholder='Committee Address ' />
+                            </Form.Item>
+
+                            <MinusCircleOutlined
+                              style={{ fontSize: '20px' }}
+                              className='dynamic-delete-button mb-6 mr-6'
+                              onClick={() => {
+                                if (fields.length < 3) {
+                                  message.error(
+                                    'At least three committee members must be ensured.',
+                                  )
+                                } else {
+                                  remove(index)
+                                }
+                              }}
+                            />
+                          </div>
+                        )
+                      })}
+                    </div>
+                    <Form.Item>
+                      <Button onClick={() => add()} icon={<PlusOutlined />}>
+                        Add committee
+                      </Button>
+                    </Form.Item>
+                  </>
+                )}
+              </Form.List>
+            )}
             <Button loading={isSubmitting} type='primary' htmlType='submit'>
               Change Committee
             </Button>
