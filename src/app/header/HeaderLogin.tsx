10a267bb093e1f45f38f8c2c2baec11d5c32645a
alexsunxl <alexsunxl@163.com>
2024-08-05 14:59:22 +0800

    token add reload

--- src/app/header/HeaderLogin.tsx
+++ src/app/header/HeaderLogin.tsx
@@ -5,25 +5,34 @@ import HeaderUserAvatar from './HeaderUserAvatar'
 import { useAsyncEffect } from 'ahooks'
 import { getTokenContract } from '@contracts/index'
 import { formatUnits } from 'ethers'
-import { Tag } from 'antd'
+import { Tag, Spin } from 'antd'
 
 const HeaderRight = () => {
   const user = useUserStore()
+  const [loading, setLoading] = useState<boolean>(false)
   const [tokenAmount, setTokenAmount] = useState<string>('')
 
-  useAsyncEffect(async () => {
+  const reload = async () => {
     if (user.user.address) {
+      setLoading(true)
       const instance = await getTokenContract()
       const token = await instance.balanceOf(user.user.address)
       console.log('token', token)
       setTokenAmount(formatUnits(token, 18))
+      setLoading(false)
     }
+  }
+
+  useAsyncEffect(async () => {
+    await reload()
   }, [user])
+
   return (
     <>
       <div className='flex-center gap-2'>
-        <div className='flex-center'>
-          <div>{tokenAmount ? tokenAmount : 0}</div>
+        <div className='flex-center' onClick={() => reload()}>
+          {loading && <Spin size='small' />}
+          {!loading && <div>{tokenAmount ? tokenAmount : 0}</div>}
           <Tag>BDT</Tag>
         </div>
         <HeaderUserAvatar />
