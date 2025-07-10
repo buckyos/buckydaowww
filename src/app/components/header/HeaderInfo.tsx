02a08604174bd516464575893543e021542af439
alexsunxl <alexsunxl@163.com>
2025-07-10 17:05:59 +0800

    Fix

--- src/app/components/header/HeaderInfo.tsx
+++ src/app/components/header/HeaderInfo.tsx
@@ -8,37 +8,58 @@ import { formatUnits } from 'ethers'
 import { Tag, Spin } from 'antd'
 
 const HeaderInfo = () => {
-  const user = useUserStore()
-  const [loading, setLoading] = useState<boolean>(false)
-  const [tokenAmount, setTokenAmount] = useState<string>('')
+     const user = useUserStore()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [devTokenAmount, setDevTokenAmount] = useState<string>('')
+    const [normalTokenAmount, setNormalTokenAmount] = useState<string>('')
 
-  const reload = async () => {
-    if (user.user.address) {
-      setLoading(true)
-      const instance = await contractService.getDevTokenContract()
-      const token = await instance.balanceOf(user.user.address)
-      console.log('token', token)
-      setTokenAmount(parseFloat(formatUnits(token, 18)).toFixed(2))
-      setLoading(false)
+    const reload = async () => {
+        if (user.user.address) {
+            setLoading(true)
+            const devToken = await contractService.getDevTokenContract()
+            const normalToken = await contractService.getNormalTokenContract()
+            const token = await Promise.all([
+                devToken.balanceOf(user.user.address),
+                normalToken.balanceOf(user.user.address)
+            ])
+            console.log('token', token)
+            setDevTokenAmount(parseFloat(formatUnits(token[0], 18)).toFixed(2))
+            setNormalTokenAmount(parseFloat(formatUnits(token[1], 18)).toFixed(2))
+            setLoading(false)
+        }
     }
-  }
 
-  useAsyncEffect(async () => {
-    await reload()
-  }, [user])
+    useAsyncEffect(async () => {
+        await reload()
+    }, [user])
 
-  return (
-    <>
-      <div className='flex-center gap-2'>
-        <div className='flex-center' onClick={() => reload()}>
-          {loading && <Spin size='small' />}
-          {!loading && <div>{tokenAmount ? tokenAmount : 0}</div>}
-          <Tag>BDT</Tag>
-        </div>
-        <HeaderUserAvatar />
-      </div>
-    </>
-  )
+
+    function TokenInfo() {
+        return (
+            <div className='flex-center flex-col gap-2'>
+                <div className='flex-center gap-1'>
+                    <div>{devTokenAmount ? devTokenAmount : 0}</div>
+                    <Tag>BDDT</Tag>
+                </div>
+                <div className='flex-center gap-1'>
+                    <div>{normalTokenAmount ? normalTokenAmount : 0}</div>
+                    <Tag>BDT</Tag>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <div className='flex-center gap-2'>
+                <div className='flex-center' onClick={() => reload()}>
+                    {loading && <Spin size='small' />}
+                    {!loading && <TokenInfo />}
+                </div>
+                <HeaderUserAvatar />
+            </div>
+        </>
+    )
 }
 
 export default HeaderInfo
