1be0efd41d02224a8e0641b0f385aef2c89aa4af
alexsunxl <alexsunxl@163.com>
2025-07-11 15:21:43 +0800

    update invest

--- src/app/components/home/FundingEntrance.tsx
+++ src/app/components/home/FundingEntrance.tsx
@@ -1,66 +1,11 @@
 'use client'
-import cx from 'classnames'
-import { InvestmentState } from '@vars/index'
-import { Button, message } from 'antd'
-import { nowTimestamp, timeago } from '@utils/time'
-import { useRouter } from 'next/navigation'
-import { useInvestment } from '@hooks/useInvestment'
+import { Button } from 'antd'
 import Link from 'next/link'
 
 const FundingEntrance = () => {
-  const { latestInvestment } = useInvestment()
-
-  const latestInvestmentEndTimeDisplay = () => {
-    if (latestInvestment === null) {
-      return ''
-    }
-
-    const prefix =
-      latestInvestment.endTime > nowTimestamp() ? 'Will end' : 'Ended about'
-    return `${prefix} ${timeago(latestInvestment.endTime * 1000)}`
-  }
-
-  const latestInvestmentTagColorClass = () => {
-    if (latestInvestment === null) {
-      return 'bg-gray-400'
-    }
-
-    switch (latestInvestment.state) {
-      case InvestmentState.PREPARE:
-        return 'bg-[#FAAD14]'
-      case InvestmentState.STARTED:
-        return 'bg-[#1890FF]'
-      case InvestmentState.SUCCESSFUL:
-        return 'bg-[#52C41A]'
-      case InvestmentState.FAILED:
-        return 'bg-[#F5222D]'
-      default:
-        return 'bg-gray-400'
-    }
-  }
-
   return (
     <div className='border-round p-4 mt-10'>
       <div className='flex justify-between items-center'>
-        <div className='flex items-center gap-6 ml-3'>
-          <div
-            className={cx(
-              'w-3 h-3',
-              'rounded-md',
-              `${latestInvestmentTagColorClass()}`,
-            )}
-          />
-          <div className='flex flex-col justify-center'>
-            <div className='font-medium'>
-              {latestInvestment?.title
-                ? latestInvestment?.title
-                : 'Latest Investment'}
-            </div>
-            <div className='text-sm text-black-secondary'>
-              {latestInvestmentEndTimeDisplay()}
-            </div>
-          </div>
-        </div>
         <Link href='/invest'>
           <Button type='primary' >
             Funding Now
