da84f1d8ca5b168b45d7254c62dff3189bf62dc8
alexsunxl <alexsunxl@163.com>
2025-07-11 16:09:11 +0800

    style

--- src/app/header/nav.tsx
+++ src/app/header/nav.tsx
@@ -1,15 +1,13 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import { SwapRightOutlined } from '@ant-design/icons'
 import Link from 'next/link'
 
 const Nav = () => {
-  const router = useRouter()
   return (
     <nav className='flex flex-col items-start gap-4 md:flex-row md:items-center md:gap-[38px]'>
       <Link
         href="/governance_introducing"
-        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer flex items-center gap-2'
+        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer flex items-center gap-2 no-underline'
       >
         <div>Governance</div>
         <div className='-rotate-45'>
@@ -19,27 +17,27 @@ const Nav = () => {
 
       <Link
         href="/#members"
-        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer'
+        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer no-underline'
       >
         Member
       </Link>
       <Link
         href="/proposals"
-        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer'
+        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer no-underline'
       >
         Proposal
       </Link>
 
       <Link
         href="/invest"
-        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer'
+        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer no-underline'
       >
         Investment
       </Link>
 
       <Link
         href="/projects"
-        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer'
+        className='text-base font-medium text-black-primary hover:text-black-secondary cursor-pointer no-underline'
       >
         Project
       </Link>
