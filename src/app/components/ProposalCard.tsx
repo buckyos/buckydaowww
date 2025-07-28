71d543bfb9ebf97e094a43b41f9c3b05755609d2
alexsunxl <alexsunxl@163.com>
2025-07-28 16:31:52 +0800

    提案列表的进度条是错误的（全局投票）先全部隐藏

--- src/app/components/ProposalCard.tsx
+++ src/app/components/ProposalCard.tsx
@@ -76,7 +76,7 @@ const ProposalCard: React.FC<ProposalCardProps> = ({ item, memberCount }) => {
           {proposalExtraDisplay()}
         </div>
         <div>
-          <div className='flex items-baseline gap-1 text-xs'>
+          {/* <div className='flex items-baseline gap-1 text-xs'>
             <Progress percent={supportPercent()} showInfo={false} />
             <div>{item.supportCount}</div>
             <div>agree</div>
@@ -86,7 +86,7 @@ const ProposalCard: React.FC<ProposalCardProps> = ({ item, memberCount }) => {
             <Progress percent={rejectPercent()} showInfo={false} />
             <div>{item.rejectCount}</div>
             <div>disagree</div>
-          </div>
+          </div> */}
         </div>
 
         <div className='text-sm text-black-secondary'>
