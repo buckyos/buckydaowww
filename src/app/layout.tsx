bf53ad16145e2f28f3e3fc34f3765bd753016bf2
liyaxing <lurenpluto@gmail.com>
2026-03-24 08:33:59 -0700

    Warn when local chain time drifts far ahead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import Header from './header/header'
 import Fetcher from 'header/Fetcher'
 import Footer from './footer'
+import LocalChainTimeNotice from './header/LocalChainTimeNotice'
 import localFont from 'next/font/local'
 
 // Font files can be colocated inside of `app`
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={font.className}>
         <Fetcher />
         <Header />
+        <LocalChainTimeNotice />
         <main className='max-w-[1260px] mx-auto min-h-[calc(100vh-180px)]'>
           {children}
         </main>
